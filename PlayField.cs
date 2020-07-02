ad22f68639741dcfba60866f4a7c32fff61e87f0
BuildTools <unconfigured@null.spigotmc.org>
2020-07-02 07:33:33 +0200

    Added Playfield

---
+++ PlayField.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class PlayField
+    {
+        private cell[,] cells;
+        private byte x;
+        private byte y;
+
+        public PlayField(byte y, byte x)
+        {
+            this.x = x;
+            this.y = y;
+            cells = new cell[y, x];
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    cells[i, j] = new cell();
+                }
+            }
+        }
+    }
+}
